ece5fb6bf400eb6e25e040cce32639ef37dbbdf1
Ting Wei Wang <eltonwang1@gmail.com>
2024-05-03 14:46:09 +0800

    Add files via upload

---
+++ Scientific Computing with Python/Polygon Area Calculator/main.py
@@ -0,0 +1,51 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def set_width(self, width):
+        self.width = width
+
+    def set_height(self, height):
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return 2 * (self.width + self.height)
+
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** .5
+
+    def get_picture(self):
+        if self.width > 50 or self.height > 50:
+            return "Too big for picture."
+        picture = ""
+        for _ in range(self.height):
+            picture += "*" * self.width + "\n"
+        return picture
+
+    def get_amount_inside(self, shape):
+        return self.get_area() // shape.get_area()
+
+    def __str__(self):
+        return f"Rectangle(width={self.width}, height={self.height})"
+
+
+class Square(Rectangle):
+    def __init__(self, side):
+        super().__init__(side, side)
+
+    def set_side(self, side):
+        self.width = side
+        self.height = side
+
+    def set_width(self, side):
+        self.set_side(side)
+
+    def set_height(self, side):
+        self.set_side(side)
+
+    def __str__(self):
+        return f"Square(side={self.width})"
